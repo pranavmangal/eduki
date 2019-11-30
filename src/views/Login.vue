23be3fcc4c31db5bab3478beeb119e719fc5f97e
Pranav Mangal <pranav.mangal18@imperial.ac.uk>
2019-11-30 19:01:36 +0000

    Added input validation

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,8 +4,20 @@
       <v-layout align-center>
         <v-flex fill-height>
           <h2 style="margin-bottom: 5%">Sign In</h2>
-          <v-text-field type="text" label="Email" outlined v-model="email"></v-text-field>
-          <v-text-field type="password" label="Password" outlined v-model="password"></v-text-field>
+          <v-text-field
+            type="text"
+            label="Email"
+            :rules="[rules.required, rules.email]"
+            v-model="email"
+            outlined
+          ></v-text-field>
+          <v-text-field
+            type="password"
+            label="Password"
+            :rules="[rules.required]"
+            v-model="password"
+            outlined
+          ></v-text-field>
           <v-btn color="primary" @click="login">Login</v-btn>
         </v-flex>
       </v-layout>
@@ -21,7 +33,14 @@ export default {
   data() {
     return {
       email: "",
-      password: ""
+      password: "",
+      rules: {
+        required: value => !!value || "Required.",
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "Invalid e-mail.";
+        }
+      }
     };
   },
   methods: {
