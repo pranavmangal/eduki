102b4441032170e5b1acd3a2718771fe6abf3a0f
Hasan Ali <hasali20@outlook.com>
2019-11-30 15:19:44 +0000

    Add basic authentication

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,38 @@
 <template>
   <div class="login">
     <h3>Sign In</h3>
-    <input type="text" placeholder="Email" />
+    <input type="text" placeholder="Email" v-model="email" />
     <br />
-    <input type="password" placeholder="Password" />
+    <input type="password" placeholder="Password" v-model="password" />
     <button @click="login">Login</button>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
+
 export default {
   name: "login",
   data() {
-    return {};
+    return {
+      email: "",
+      password: ""
+    };
   },
   methods: {
     login: function() {
-      window.console.log("Hello, world!");
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(
+          function(user) {
+            window.console.log(user);
+            alert(`${user} is logged in`);
+          },
+          function(err) {
+            window.console.log("An error occurred.", err);
+          }
+        );
     }
   }
 };
